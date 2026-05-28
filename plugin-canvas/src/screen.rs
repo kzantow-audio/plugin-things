e01bbbc2c24468d325041b331e0a19e74c033c58
Jussi Viiri <ilmai@iki.fi>
2026-05-28 19:59:11 +0300

    Implement screen_scale() for MacOS

--- plugin-canvas/src/screen.rs
+++ plugin-canvas/src/screen.rs
@@ -1,3 +1,20 @@
+// Panics if not called from the main thread
+#[cfg(target_os = "macos")]
+pub fn screen_scale() -> f64 {
+    use objc2::MainThreadMarker;
+    use objc2_app_kit::NSScreen;
+
+    let Some(main_thread_marker) = MainThreadMarker::new() else {
+        panic!("screen_scale() must be called from the main thread");
+    };
+
+    if let Some(screen) = NSScreen::mainScreen(main_thread_marker) {
+        screen.backingScaleFactor()
+    } else {
+        1.0
+    }
+}
+
 #[cfg(target_os = "windows")]
 pub fn screen_scale() -> f64 {
     // Could use `GetDpiForWindow` here, but that's only available from Windows 10
